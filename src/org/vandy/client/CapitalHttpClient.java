00cf7847eab00c8f6c3cc17779090a30c351e105
Jordan Buckmaster <buckmast@purdue.edu>
2016-11-12 08:49:34 -0600

    fixed postCustomer and vandy build paths

--- src/org/vandy/client/CapitalHttpClient.java
+++ src/org/vandy/client/CapitalHttpClient.java
@@ -3,9 +3,7 @@ package org.vandy.client;
 import java.io.*;
 import java.util.*;
 
-import org.apache.http.Consts;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -14,7 +12,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+
 import org.json.*;
 
 public class CapitalHttpClient {
@@ -23,7 +21,8 @@ public class CapitalHttpClient {
 	private final static String apiKey = "9d7d244b2a2df641310e877e3cc45869";
 	
 	public static void main(String[] args) throws Exception {
-		postAccount("0", "Credit Card", "Test", 100, 15000, "1234567890987654");
+		//postAccount("0", "Credit Card", "Test", 100, 15000, "1234567890987654");
+		postCustomer("Jordan", "Buckmaster", "3333", "Willow", "Chicago", "IL", "12345");
 		getAccounts("Savings");
 	}
 	
@@ -46,9 +45,6 @@ public class CapitalHttpClient {
 	    
 
 	    HttpResponse response = client.execute(post);
-
-	    //StatusLine status = hres.getStatusLine(); 
-	    //System.out.println(status);
 	    
 	    System.out.println("\nSending 'POST' request to URL : " + url);
 		System.out.println("Post parameters : " + post.getEntity());
@@ -68,15 +64,15 @@ public class CapitalHttpClient {
 	}
 	
 	
-	public void postCustomer(String first, String last, String streetNum, String streetName,
+	public static void postCustomer(String first, String last, String streetNum, String streetName,
 							 String city, String state, String zip) throws Exception{
 		String url = "http://api.reimaginebanking.com/customers?key=" + apiKey;
 		HttpPost post = new HttpPost(url);
 		HttpClient client = HttpClients.createDefault();
 		JSONObject juo = new JSONObject();
 		JSONObject nestedJUO = new JSONObject();
-		juo.put("type", first);
-		juo.put("nickname", last);
+		juo.put("first_name", first);
+		juo.put("last_name", last);
 		
 		nestedJUO.put("street_number", streetNum);
 		nestedJUO.put("street_name", streetName);
@@ -242,73 +238,4 @@ public class CapitalHttpClient {
 		if(!printed)
 			System.out.println("Account not found.");
 	}
-	// HTTP GET request 
-/*	private void sendGet() throws Exception {
-
-		String url = "http://www.google.com/search?q=developer";
-		
-		DefaultHttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-
-		// add request header
-		request.addHeader("User-Agent", USER_AGENT);
-
-		HttpResponse response = client.execute(request);
-
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " +
-                       response.getStatusLine().getStatusCode());
-
-		BufferedReader rd = new BufferedReader(
-                       new InputStreamReader(response.getEntity().getContent()));
-
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-
-		System.out.println(result.toString());
-
-	}
-
-	// HTTP POST request
-	private void sendPost() throws Exception {
-
-		String url = "https://selfsolve.apple.com/wcResults.do";
-
-		//DefaultHttpClient client = new DefaultHttpClient();
-		HttpPost post = new HttpPost(url);
-
-		// add headers
-		
-		
-		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-		urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
-		urlParameters.add(new BasicNameValuePair("cn", ""));
-		urlParameters.add(new BasicNameValuePair("locale", ""));
-		urlParameters.add(new BasicNameValuePair("caller", ""));
-		urlParameters.add(new BasicNameValuePair("num", "12345"));
-
-		post.setEntity(new UrlEncodedFormEntity(urlParameters));
-
-		//HttpResponse response = client.execute(post);
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + post.getEntity());
-		System.out.println("Response Code : " +
-                                    response.getStatusLine().getStatusCode());
-
-		BufferedReader rd = new BufferedReader(
-                        new InputStreamReader(response.getEntity().getContent()));
-
-		StringBuffer result = new StringBuffer();
-		String line = "";
-		while ((line = rd.readLine()) != null) {
-			result.append(line);
-		}
-
-		System.out.println(result.toString());
-
-	}*/
-
 }
