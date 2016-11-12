2b2edfba667dfa3e31516ff305ff4509a7054cf1
patel627 <patel627@purdue.eduL>
2016-11-12 10:53:32 -0600

    getallcustomers

--- src/org/vandy/client/CapitalHttpClient.java
+++ src/org/vandy/client/CapitalHttpClient.java
@@ -422,4 +422,36 @@ public class CapitalHttpClient {
 		if(!printed)
 			System.out.println("Account not found.");
 	}
+
+	public static String[] getAllCustomers() throws Exception {
+		String url = "http://api.reimaginebanking.com/customers/?key="+apiKey;
+		HttpClient client = HttpClients.createDefault();
+		HttpGet request = new HttpGet(url);
+		request.addHeader("Accept", "application/json");
+		HttpResponse response = client.execute(request);
+		
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Response Code : " +
+                                    response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		System.out.println(result.toString());
+		
+		JSONArray arr = new JSONArray(result.toString());
+		String[] customerIds = new String[arr.length()];
+		for (int i = 0; i<arr.length(); i++) {
+			customerIds[i] = arr.getJSONObject(i).getString("_id");
+		}
+		
+		return customerIds;
+
+	}
+	
 }
