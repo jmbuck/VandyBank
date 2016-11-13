45210b432b3cd66e9805201f7432ca59867eb2d9
patel627 <patel627@purdue.eduL>
2016-11-13 08:02:47 -0600

    fix

--- src/org/vandy/client/Bank.java
+++ src/org/vandy/client/Bank.java
@@ -290,7 +290,7 @@ public class Bank {
 	public static String[] getAccountNums() {
 		String[] acctNums = new String[accountList.size()];
 		for (int i = 0; i<accountList.size(); i++) {
-			acctNums[i] = accountList.get(0).getCustomerID();
+			acctNums[i] = accountList.get(i).getCustomerID();
 		}
 		return acctNums;
 	}
