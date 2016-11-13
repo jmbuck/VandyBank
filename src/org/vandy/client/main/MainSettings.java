b530c553c5fd6fbd4f0f36d6b3ebf14390cac66b
Killian-LeClainche <kleclain@purdue.edu>
2016-11-13 09:52:43 -0600

    Made all remaining content under construction.

--- src/org/vandy/client/main/MainSettings.java
+++ src/org/vandy/client/main/MainSettings.java
@@ -1,11 +1,24 @@
 package org.vandy.client.main;
 
+import org.lwjgl.opengl.GL11;
+
 public class MainSettings extends MainState
 {
 
 	public MainSettings(MainGui gui, boolean alt)
 	{
 		super(gui, alt);
+	}
+	
+	public void render(double delta)
+	{
+		super.render(delta);
+		
+		boldFont.bind();
+		boldFont.draw("UNDER CONSTRUCTION!", 1920 / 2 - boldFont.getWidth("UNDER CONSTRUCTION!", .5f) / 2, 1080 / 2, 0, .5f);
+		boldFont.unbind();
+		
+		GL11.glPopMatrix();
 	}
 
 }
