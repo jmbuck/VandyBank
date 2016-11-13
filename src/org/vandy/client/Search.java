bab3c197f574dcb0dbdd4d3587f9945f87794465
patel627 <patel627@purdue.eduL>
2016-11-12 23:10:49 -0600

    using bank methods

--- src/org/vandy/client/Search.java
+++ src/org/vandy/client/Search.java
@@ -8,23 +8,23 @@ public class Search {
 	
 	public String[] doSearch(int n) throws Exception {
 		String[] retArr = new String[n];
-		String[] userIds = CapitalHttpClient.getAllCustomers();
+		String[] userIds = Bank.getCustomers();
 		if (userIds.length<=n) {
 			for (int i = 0; i < userIds.length; i++) {
-				retArr[i] = CapitalHttpClient.getCustomerByID(userIds[i], "first_name") + " " +
-						CapitalHttpClient.getCustomerByID(userIds[i], "last_name");
+				retArr[i] = Bank.lookUpCustomer(userIds[i], "first_name") + " " +
+						Bank.lookUpCustomer(userIds[i], "last_name");
 			}
 			return retArr;
 		}
 		
 		int[] diffs = new int[userIds.length];
 		for (int i = 0; i<diffs.length; i++) {
-			diffs[i] = calcSimilarity(phrase, CapitalHttpClient.getCustomerByID(userIds[i], "first_name")) * 2 +
-					calcSimilarity(phrase, CapitalHttpClient.getCustomerByID(userIds[i], "last_name"));
+			diffs[i] = calcSimilarity(phrase, Bank.lookUpCustomer(userIds[i], "first_name")) * 2 +
+					calcSimilarity(phrase, Bank.lookUpCustomer(userIds[i], "last_name"));
 		}
 		for (int i = 0; i<n; i++) {
-			retArr[i] = CapitalHttpClient.getCustomerByID(userIds[getIndexOfLowest(diffs)], "first_name") + " " +
-					CapitalHttpClient.getCustomerByID(userIds[getIndexOfLowest(diffs)], "last_name");
+			retArr[i] = Bank.lookUpCustomer(userIds[getIndexOfLowest(diffs)], "first_name") + " " +
+					Bank.lookUpCustomer(userIds[getIndexOfLowest(diffs)], "last_name");
 			diffs[getIndexOfLowest(diffs)] = Integer.MAX_VALUE;
 		}
 		
