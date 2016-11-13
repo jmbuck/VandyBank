b9a92f3f08760c20a34afbbf23b7a3d68dc9316e
JordanR159 <reed226@purdue.edu>
2016-11-13 11:22:50 -0500

    api

--- src/org/vandy/client/CapitalHttpClient.java
+++ src/org/vandy/client/CapitalHttpClient.java
@@ -17,7 +17,7 @@ import org.json.*;
 
 public class CapitalHttpClient {
 
-	//private final String USER_AGENT = "Mozilla/5.0";
+	//private final String USER_AGENT = "Mozilla/5.0";//
 	private final static String apiKey = "be1825b31260b22a7078277116ab829e";
 	public static void main(String[] args) {
 		
